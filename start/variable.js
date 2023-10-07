dae0a365d047319d4db04b79b97b158639c55510
chandan195 <97842262+chandan195@users.noreply.github.com>
2023-10-07 14:39:02 +0000

    variable in javascript

---
+++ start/variable.js
@@ -0,0 +1,13 @@
+//* var, let ,const
+const userId=354;
+var userName ="chandan";
+let UserPassWord="109isk";
+usercity="mirzapur" // not a good way
+let userState;
+// user define variable and not provide  value   ,so  get //*undefine in result
+userName="chandan maurya";
+UserPassWord="ch12iak";
+usercity ="Mirzapur Utter Pradesh";
+console.log( userId);
+
+console.table([userName,usercity,UserPassWord,userState])
