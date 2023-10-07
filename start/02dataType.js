8a529d03b72bd0935f572a0744d8835644c455c2
chandan195 <97842262+chandan195@users.noreply.github.com>
2023-10-07 15:11:35 +0000

    data type

---
+++ start/02dataType.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as newer version
+ //alert(3+3); //! we are using node js not brower
+ 
+
+ /*
+  number 
+  bigint
+  string
+  boolean
+  null
+  undefine
+  symbol
+ */
+
+  //! object
+
+  console.log( typeof undefined);   //* undefine
+  console.log(typeof null )       //! object
